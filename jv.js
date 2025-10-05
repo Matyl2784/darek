39af23623cfe2c71a345f75ac021eaad3e7a6867
Matyl <matyas.hanus@gmail.com>
2025-10-05 14:53:47 +0000

    oprava?

--- jv.js
+++ jv.js
@@ -21,48 +21,40 @@ const firebaseConfig = {
 };
 
 
-// Inicializace Firebase
-firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
-
-// uložíme čas načtení
-const startTime = Date.now();
-
-// vytvoříme dokument pro tuto návštěvu
-let visitRef = db.collection("visits").doc(); // Firestore automaticky vytvoří unikátní ID
-
-// první zápis při načtení
-visitRef.set({
-    url: window.location.href,
-    startTime: startTime,
-    lastUpdate: Date.now(),
-    timeSpent: 0
-});
-
-// každých 5 sekund aktualizujeme timeSpent
-const intervalId = setInterval(() => {
-    const currentTime = Date.now();
-    const secondsSpent = Math.round((currentTime - startTime) / 1000);
-
-    visitRef.update({
-        lastUpdate: currentTime,
-        timeSpent: secondsSpent
-    }).catch(err => console.error("Chyba při aktualizaci:", err));
-}, 5000);
-
-// volitelně, při odchodu z okna - finální update
-window.addEventListener('beforeunload', function () {
-    clearInterval(intervalId); // zastav interval
-    const endTime = Date.now();
-    const secondsSpent = Math.round((endTime - startTime) / 1000);
-
-    visitRef.update({
-        lastUpdate: endTime,
-        timeSpent: secondsSpent
+function initFirebase() {
+    firebase.initializeApp(firebaseConfig);
+    const db = firebase.firestore();
+    let visitRef = db.collection("visits").doc();
+    const startTime = Date.now();
+    visitRef.set({
+        url: window.location.href,
+        startTime: startTime,
+        lastUpdate: Date.now(),
+        timeSpent: 0
     });
-});
+    const intervalId = setInterval(() => {
+        const currentTime = Date.now();
+        const secondsSpent = Math.round((currentTime - startTime) / 1000);
+        visitRef.update({
+            lastUpdate: currentTime,
+            timeSpent: secondsSpent
+        }).catch(err => console.error(err));
+    }, 5000);
+    window.addEventListener('beforeunload', () => {
+        clearInterval(intervalId);
+        const endTime = Date.now();
+        const secondsSpent = Math.round((endTime - startTime)/1000);
+        visitRef.update({
+            lastUpdate: endTime,
+            timeSpent: secondsSpent
+        });
+    });
+}
+
 
 alert("Aloha starouši, pro pokračování se ujisti že máš zapnutý zvuk, díky!");
+
+initFirebase()
 
 // now we will setup our basic variables for the demo
 var canvas = document.getElementById( 'canvas' ),
